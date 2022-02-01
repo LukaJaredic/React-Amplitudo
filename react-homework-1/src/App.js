5cf982bf40df267030a2566ebb6aa839e24f0b18
Luka Jaredic <projaredic@gmail.com>
2022-02-01 07:59:44 +0100

    used smarter and cleaner way of generating components, array.map->components

--- react-homework-1/src/App.js
+++ react-homework-1/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'
 import './App.css'
 import { Lista } from './components/Lista/Lista'
 import { LicnaKarta } from './components/LicnaKarta/LicnaKarta'
@@ -7,78 +6,119 @@ import { Knjiga } from './components/Knjiga/Knjiga'
 import { Film } from './components/Film/Film'
 
 function App() {
+    const celebrities = [
+        {
+            name: 'Ronaldo',
+            surname: 'de Assis Moreira',
+            dob: '21/3/1980',
+            city: 'Porto Alegre',
+            imgSrc: 'images/ronaldinjo.jpg',
+            imgAlt: 'Ronaldinho in sweater',
+        },
+        {
+            name: 'Mikhail',
+            surname: 'Tal',
+            dob: '9/11/1936',
+            city: 'Riga',
+            imgSrc: 'images/tal.jfif',
+            imgAlt: 'Tal playing chess',
+        },
+        {
+            name: 'Gautama',
+            surname: 'Buddha',
+            dob: '6th centry BCE',
+            city: 'Lumbini',
+            imgSrc: 'images/buddha.jpg',
+            imgAlt: 'Stone statue of Buddha',
+        },
+    ]
+    const books = [
+        {
+            title: 'Power of now',
+            year: 2004,
+            author: 'Eckart Tolle',
+            quote: 'Emotions arise in the place where your mind and body meet',
+            imgSrc: 'images/powerOfNow.jfif',
+            imgAlt: 'lightblue book cover',
+        },
+        {
+            title: 'The untethered soul',
+            year: 2007,
+            author: 'Michael A. Singer',
+            quote: 'Only you can take inner freedom away from yourself, or give it to yourself.',
+            imgSrc: 'images/untetheredSoul.jfif',
+            imgAlt: 'Lightblue book cover with a horse on it',
+        },
+        {
+            title: 'The Brothers Karamazov',
+            year: 1879,
+            author: 'Fyodor Dostoevsky',
+            quote: "Above all, don't lie to yourself. The man who lies to himself and listens to his own lie comes to a point that he cannot distinguish the truth within him, or around him, and so loses all respect for himself and for others. And having no respect he ceases to love.",
+            imgSrc: 'images/brothersKaramazov.jfif',
+            imgAlt: 'Book cover with 2 men on it',
+        },
+    ]
+    const movies = [
+        {
+            title: '12 angry men',
+            year: 1957,
+            ganre: 'Drama',
+            author: 'Reginald Rose',
+            actors: 'Henry Fonda, Jack Klugman',
+            imgSrc: 'images/12angryMen.jfif',
+            imgAlt: 'black and white movie poster',
+        },
+        {
+            title: 'Pulp Fiction',
+            year: 1994,
+            ganre: 'Crime/Drama',
+            author: ' Quentin Tarantino',
+            actors: 'Uma Thruman, Samuel L. Jackson',
+            imgSrc: 'images/pulpFiction.jfif',
+            imgAlt: 'Colorful movie poster',
+        },
+        {
+            title: 'Step up',
+            year: 2006,
+            ganre: 'Dance/Musical',
+            author: 'Anne Fletcher',
+            actors: 'Channing Tatum, Drew Sigora',
+            imgSrc: 'images/stepUp.jfif',
+            imgAlt: 'Movie poster of a man and a woman dancing',
+        },
+    ]
+
+    const celebrityList = celebrities.map((celebrity) => {
+        return (
+            <LicnaKarta name={celebrity.name} surname={celebrity.surname} dob={celebrity.dob} city={celebrity.city}>
+                <Slika src={celebrity.imgSrc} alt={celebrity.imgAlt} />
+            </LicnaKarta>
+        )
+    })
+    const bookList = books.map((book) => {
+        return (
+            <Knjiga title={book.title} year={book.year} author={book.author} quote={book.quote}>
+                <Slika src={book.imgSrc} alt={book.imgAlt} />
+            </Knjiga>
+        )
+    })
+
+    const movieList = movies.map((movie) => {
+        return (
+            <Film title={movie.title} year={movie.year} ganre={movie.ganre} author={movie.author} actors={movie.actors}>
+                <Slika src={movie.imgSrc} alt={movie.imgAlt} />
+            </Film>
+        )
+    })
     return (
         <div className='App'>
             <Lista>
                 <h1 class='main__heading'>Celebrities:</h1>
-                <LicnaKarta name='Ronaldo' surname='de Assis Moreira' dob='21/3/1980' city='Porto Alegre'>
-                    <Slika src='images/ronaldinjo.jpg' alt='Young Ronaldiho in black and white sweater' />
-                </LicnaKarta>
-                <LicnaKarta name='Mikhail' surname='Tal' dob='9/11/1936' city='Riga'>
-                    <Slika src='images/tal.jfif' alt='Young tall playing chess' />
-                </LicnaKarta>
-                <LicnaKarta name='Gautama' surname='Buddha' dob='6th centry BCE' city='Lumbini'>
-                    <Slika src='images/buddha.jpg' alt='A stone statue of Buddha meditating' />
-                </LicnaKarta>
-
+                {celebrityList}
                 <h1 class='main__heading'>Books:</h1>
-
-                <Knjiga
-                    title='Power of now'
-                    year={2004}
-                    author='Eckart Tolle'
-                    quote='Emotions arise in the place where your mind and body meet'
-                >
-                    <Slika src='images/powerOfNow.jfif' alt='The power of now cover' />
-                </Knjiga>
-                <Knjiga
-                    title='The untethered soul'
-                    year={2007}
-                    author='Michael A. Singer'
-                    quote='Only you can take inner freedom away from yourself, or give it to yourself.'
-                >
-                    <Slika src='images/untetheredSoul.jfif' alt='The untethered soul cover' />
-                </Knjiga>
-                <Knjiga
-                    title='The Brothers Karamazov'
-                    year={1879}
-                    author='Fyodor Dostoevsky'
-                    quote="Above all, don't lie to yourself. The man who lies to himself and listens to his own lie comes to a point that he cannot distinguish the truth within him, or around him, and so loses all respect for himself and for others. And having no respect he ceases to love."
-                >
-                    <Slika src='images/brothersKaramazov.jfif' alt='brothers karamazov cover' />
-                </Knjiga>
-
+                {bookList}
                 <h1 className='meain__heading'>Movies: </h1>
-
-                <Film
-                    title='12 angry men'
-                    year={1957}
-                    ganre='Drama'
-                    author='Reginald Rose'
-                    actors='Henry Fonda, Jack Klugman'
-                >
-                    <Slika src='images/12angryMen.jfif' alt='12 angry men cover' />
-                </Film>
-
-                <Film
-                    title='Pulp Fiction'
-                    year={1994}
-                    ganre='Crime/Drama'
-                    author=' Quentin Tarantino'
-                    actors='Uma Thruman, Samuel L. Jackson'
-                >
-                    <Slika src='images/pulpFiction.jfif' alt='Pulp Fiction cover' />
-                </Film>
-
-                <Film
-                    title='Step up'
-                    year={2006}
-                    ganre='Dance/Musical'
-                    author='Anne Fletcher'
-                    actors='Channing Tatum, Drew Sigora'
-                >
-                    <Slika src='images/stepUp.jfif' alt='Step up cover' />
-                </Film>
+                {movieList}
             </Lista>
         </div>
     )
