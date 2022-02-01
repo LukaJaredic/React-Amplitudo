5b1c8e810086ee41bbb144bd64f4d825dab971b8
Luka Jaredic <projaredic@gmail.com>
2022-02-01 15:25:40 +0100

    second homework started, questionlist created

---
+++ react-homework-2/src/questions.js
@@ -0,0 +1,80 @@
+const questions = [
+    {
+        id: 0,
+        question: 'In programming language java, which keyword or set of keywords does not exist?',
+        answers: ['for', 'do...while...', 'loop', 'static'],
+        correct: 2,
+    },
+    {
+        id: 1,
+        question: 'What is the capital of Montenegro?',
+        answers: ['Cetinje', 'New York', 'Budapest', 'Podgorica'],
+        correct: 3,
+    },
+
+    {
+        id: 2,
+        question: '"Amplitudo" is a:',
+        answers: ['IT company', 'Weather forcast facility', 'Parfume brand', 'Famous Compton gangster'],
+        correct: 0,
+    },
+    {
+        id: 3,
+        question: 'PMF stands for:',
+        answers: ['Pre-modern figter', 'Piling money fast', 'Prirodno-matematicki fakultet', 'Please, move fast'],
+        correct: 2,
+    },
+    {
+        id: 4,
+        question: 'Delta city is:',
+        answers: ['Ancient civilization', 'Shopping mall', 'Black-ops unit', 'Casino'],
+        correct: 1,
+    },
+
+    {
+        id: 5,
+        question: 'Gintas is:',
+        answers: ['Alchoholic beverage', 'Nickname for armored vehicle', 'Shopping mall', 'City in South Africa'],
+        correct: 2,
+    },
+
+    {
+        id: 6,
+        question: 'Whoo see is:',
+        answers: [
+            'Rapper duo from Montenegro',
+            'Private clinic specialized in curing bad eyesight',
+            'Terrorist group',
+            'IT company',
+        ],
+        correct: 0,
+    },
+
+    {
+        id: 7,
+        question: 'How many continents are there?',
+        answers: ['1', '10', '5', '7'],
+        correct: 3,
+    },
+
+    {
+        id: 8,
+        question: 'Simpsons are:',
+        answers: ['Cartoon family', 'Detroit gang', 'Washington gang', 'Atalanta gank'],
+        correct: 0,
+    },
+
+    {
+        id: 9,
+        question: 'Mikri Maus is:',
+        answers: ['Cartoon character', 'Serbian rapper', 'Serbian tennis player', 'cartoon animation artist'],
+        correct: 1,
+    },
+
+    {
+        id: 10,
+        question: 'Robinson Crusoe is:',
+        answers: ['American billionaire', 'Famous singer', 'Actor', 'Book character'],
+        correct: 3,
+    },
+]
